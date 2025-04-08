8e83d9b193d4c8e11eb6ab43bd4741b98b04009b
Saad Raza <razasaad62186gmail.com>
2025-04-09 00:20:41 +0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Over the past few years, I&nbsp;ve been actively involved in full stack
+        development, constantly learning new technologies and building modern,
+        high-performance web applications. My focus lies in developing
+        optimized, scalable, and secure digital solutions — from UI development
+        to backend architecture and deployment. I&nbsp;ve worked extensively with
+        React-based stacks, particularly Next.js, and have hands-on experience
+        with headless CMS platforms like Sanity.io. I enjoy working with
+        JavaScript and TypeScript, and often use Jamstack technologies (Next.js
+        / Sanity.io / Tailwind CSS / Firebase, etc.) to create blazing-fast,
+        SEO-optimized, serverless websites that deliver a smooth and modern user
+        experience.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
